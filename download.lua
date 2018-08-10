3afa2e4939a26742ac5afb9c73e1c37596baa7df
iglak <33146259+iglak@users.noreply.github.com>
2018-08-10 18:32:54 +0200

    Update download.lua

--- download.lua
+++ download.lua
@@ -1,5 +1,5 @@
 local shell = require("shell")
 local args, ops = shell.parse(...)
 
-oc.execute("rm "..args[1])
+os.execute("rm "..args[1])
 os.execute("wget https://raw.githubusercontent.com/iglak/minecraft/master/"..args[1])
